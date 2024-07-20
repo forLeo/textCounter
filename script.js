8f57ffb9dd4050c48b9aa638fa040479692ebb54
Leo <leo.flegelskamp@t-online.de>
2024-07-20 23:27:06 +0200

    0.1

---
+++ script.js
@@ -0,0 +1,24 @@
+var textArea = document.getElementById("input");
+var words = document.getElementById("words");
+var characters = document.getElementById("characters");
+var clear = document.getElementById("clear");
+
+textArea.addEventListener("input", (event) => main());
+clear.addEventListener("click", (event) => {
+    textArea.value = "";
+    main();
+});
+
+function countWords(text) {
+    return text.split(/\s+/).filter(Boolean).length;
+}
+
+function countCharacters(text) {
+    return text.length;
+}
+
+function main() {
+    var text = textArea.value;
+    words.textContent = "words: " + countWords(text);
+    characters.textContent = "characters: " + countCharacters(text);
+}
